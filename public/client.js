8a00265c319b4941b1b1be3f23a9466b02b6c544
Kosuke Suzuki <k.suzuki764@gmail.com>
2021-03-23 02:34:23 +0900

    ユーザをDBで管理するようにした

--- public/client.js
+++ public/client.js
@@ -7,56 +7,65 @@ const socket = io.connect();
 // 　クライアント側で、'connect'イベントが発生する
 socket.on(
     'connect',
-    () =>
-    {
-        console.log( 'connect' );
-    } );
+    () => {
+        console.log('connect');
+    });
 
 // 「Join」ボタンを押したときの処理
-$( '#join-form' ).submit(
-    () =>
-    {
-        console.log( '#input_nickname :', $( '#input_nickname' ).val() );
+$('#join-form').submit(
+    () => {
+        console.log('#input_nickname :', $('#input_nickname').val());
 
-        if( $( '#input_nickname' ).val() )
-        {
+        if ($('#input_nickname').val()) {
             // サーバーに、イベント名'join' で入力テキストを送信
-            socket.emit( 'join', $( '#input_nickname' ).val() );
+            socket.emit('join', $('#input_nickname').val());
+            // ユーザ登録
+            socket.emit('User Reg', $('#input_nickname').val());
 
-            $( '#nickname' ).html( $( '#input_nickname' ).val() );
-            $( '#join-screen' ).hide();
-            $( '#chat-screen' ).show();
+            $('#nickname').html($('#input_nickname').val());
+            $('#join-screen').hide();
+            $('#chat-screen').show();
         }
 
         return false;   // フォーム送信はしない
-    } );
+    });
 
 
 // 「Send」ボタンを押したときの処理
-$( 'form' ).submit(
-    () =>
-    {
-        console.log( '#input_message :', $( '#input_message' ).val() );
+$('form').submit(
+    () => {
+        console.log('#input_message :', $('#input_message').val());
 
-        if( $( '#input_message' ).val() )
-        {
+        if ($('#input_message').val()) {
             // サーバーに、イベント名'new message' で入力テキストを送信
-            socket.emit( 'new message', $( '#input_message' ).val() );
+            socket.emit('new message', $('#input_message').val());
 
-            $( '#input_message' ).val( '' );    // テキストボックスを空に。
+            $('#input_message').val('');    // テキストボックスを空に。
         }
         return false;   // フォーム送信はしない
-    } );
+    });
+
+// 「Send」ボタンを押したときの処理(id検索)
+$('#user-search-form').submit(
+    () => {
+        console.log('#input_id :', $('#input_id').val());
+
+        if ($('#input_id').val()) {
+            socket.emit('get name', $('#input_id').val());
+
+            $('#input_id').val('');    // テキストボックスを空に。
+        }
+        return false;   // フォーム送信はしない
+    });
 
 // サーバーからのメッセージ拡散に対する処理
 // ・サーバー側のメッセージ拡散時の「io.emit( 'spread message', strMessage );」に対する処理
 socket.on(
     'spread message',
     //( strMessage ) =>
-    ( objMessage ) =>
-    {
+    (objMessage) => {
         //console.log( 'spread message :', strMessage );
-        console.log( 'spread message :', objMessage );
+        console.log('spread message :', objMessage);
 
         // メッセージの整形
         //const strText = objMessage.strDate + ' - ' + objMessage.strMessage;
@@ -64,7 +73,7 @@ socket.on(
 
         // 拡散されたメッセージをメッセージリストに追加
         //const li_element = $( '<li>' ).text( strMessage );
-        const li_element = $( '<li>' ).text( strText );
-        $( '#message_list' ).prepend( li_element ); // リストの一番上に追加
+        const li_element = $('<li>').text(strText);
+        $('#message_list').prepend(li_element); // リストの一番上に追加
         //$( '#message_list' ).append( li_element );    // リストの一番下に追加
-    } );
+    });
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 'use strict';
 
 // モジュール
-const express = require( 'express' );
-const http = require( 'http' );
-const socketIO = require( 'socket.io' );
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const mysql = require('mysql');
 
 // オブジェクト
 const app = express();
-const server = http.Server( app );
-const io = socketIO( server );
+const server = http.Server(app);
+const io = socketIO(server);
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'chat',
+    password: 'password',
+    database: 'chat'
+});
 
 // 定数
 const PORT = process.env.PORT || 1337;
@@ -16,6 +23,12 @@ const SYSTEMNICKNAME = '**system**'
 
 // グローバル変数
 let iCountUser = 0; // ユーザー数
+let maxId = 0; // DBに登録されているユーザ数
+connection.query('SELECT COUNT(*) FROM users', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results[0]['COUNT(*)']);
+    maxId = Number(results[0]['COUNT(*)']);
+});
 
 // 接続時の処理
 // ・サーバーとクライアントの接続が確立すると、
@@ -23,9 +36,8 @@ let iCountUser = 0; // ユーザー数
 // 　クライアント側で、'connect'イベントが発生する
 io.on(
     'connection',
-    ( socket ) =>
-    {
-        console.log( 'connection' );
+    (socket) => {
+        console.log('connection');
 
         let strNickname = '';	// コネクションごとで固有のニックネーム。イベントをまたいで使用される。
 
@@ -33,17 +45,15 @@ io.on(
         // ・クライアントが切断したら、サーバー側では'disconnect'イベントが発生する
         socket.on(
             'disconnect',
-            () =>
-            {
-                console.log( 'disconnect' );
+            () => {
+                console.log('disconnect');
 
-                if( strNickname )
-                {
+                if (strNickname) {
                     // ユーザー数の更新
                     iCountUser--;
 
                     // メッセージオブジェクトに現在時刻を追加
-                    const strNow = makeTimeString( new Date() );
+                    const strNow = makeTimeString(new Date());
 
                     // システムメッセージの作成
                     const objMessage = {
@@ -53,18 +63,17 @@ io.on(
                     }
 
                     // 送信元含む全員に送信
-                    io.emit( 'spread message', objMessage );
+                    io.emit('spread message', objMessage);
                 }
-            } );
+            });
 
 
         // 入室時の処理
         // ・クライアント側のメッセージ送信時の「socket.emit( 'join', strNickname );」に対する処理
         socket.on(
             'join',
-            ( strNickname_ ) =>
-            {
-                console.log( 'joined :', strNickname_ );
+            (strNickname_) => {
+                console.log('joined :', strNickname_);
 
                 // コネクションごとで固有のニックネームに設定
                 strNickname = strNickname_;
@@ -73,7 +82,7 @@ io.on(
                 iCountUser++;
 
                 // メッセージオブジェクトに現在時刻を追加
-                const strNow = makeTimeString( new Date() );
+                const strNow = makeTimeString(new Date());
 
                 // システムメッセージの作成
                 const objMessage = {
@@ -83,8 +92,8 @@ io.on(
                 }
 
                 // 送信元含む全員に送信
-                io.emit( 'spread message', objMessage );
-            } );
+                io.emit('spread message', objMessage);
+            });
 
 
 
@@ -92,12 +101,11 @@ io.on(
         // ・クライアント側のメッセージ送信時の「socket.emit( 'new message', $( '#input_message' ).val() );」に対する処理
         socket.on(
             'new message',
-            ( strMessage ) =>
-            {
-                console.log( 'new message', strMessage );
-                
+            (strMessage) => {
+                console.log('new message', strMessage);
+
                 // 現在時刻の文字列の作成
-                const strNow = makeTimeString( new Date() );
+                const strNow = makeTimeString(new Date());
 
                 // メッセージオブジェクトの作成
                 const objMessage = {
@@ -108,33 +116,105 @@ io.on(
 
                 // 送信元含む全員に送信
                 //io.emit( 'spread message', strMessage );
-                io.emit( 'spread message', objMessage );
-            } );
-    } );
+                io.emit('spread message', objMessage);
+            });
+
+        /*
+        socket.on(
+            'User Auth',
+            (strMessage) => {
+                console.log('user auth:', strMessage);
+                connection.query('SELECT name FROM users WHERE id = ?', [Number(strMessage)], function (error, results, fields) {
+                    if (error) throw error;
+                    console.log('results', results[0]['name']);
+                    // const strNow = makeTimeString(new Date());
+                    // var objMessage = {
+                    //     strNickname: '**system**',
+                    //     strMessage: results[0]['name'],
+                    //     strDate: strNow
+                    // }
+                    // io.to(socket.id).emit('spread message', objMessage);
+                    // console.log('fields: ', fields);
+                    io.to(socket.id).emit('Auth OK', "ok");
+                });
+            }
+        )
+        */
+
+        //idから名前を検索
+        socket.on(
+            'get name',
+            (strMessage) => {
+                //console.log('user auth:', strMessage);
+                if (Number(strMessage) > maxId) {
+                    console.log('results', 'not exist');
+                    const strNow = makeTimeString(new Date());
+                    var objMessage = {
+                        strNickname: '**system**',
+                        strMessage: "idが" + Number(strMessage) + "のユーザは存在しません",
+                        strDate: strNow
+                    }
+                    io.to(socket.id).emit('spread message', objMessage);
+                } else {
+                    connection.query('SELECT name FROM users WHERE id = ?', [Number(strMessage)], function (error, results, fields) {
+                        if (error) throw error;
+                        console.log('results', results[0]['name']);
+                        const strNow = makeTimeString(new Date());
+                        var objMessage = {
+                            strNickname: '**system**',
+                            strMessage: "idが" + Number(strMessage) + "のユーザの名前:" + results[0]['name'],
+                            strDate: strNow
+                        }
+                        io.to(socket.id).emit('spread message', objMessage);
+                        //console.log('fields: ', fields);
+                    });
+                }
+            }
+        )
+
+        //ユーザをDBに登録
+        socket.on(
+            'User Reg',
+            (strMessage) => {
+                console.log('user reg:', strMessage);
+                const assignId = ++maxId;
+                connection.query('INSERT users values(?, ?)', [assignId, strMessage], function (error, results, fields) {
+                    if (error) throw error;
+                    const strNow = makeTimeString(new Date());
+                    var objMessage = {
+                        strNickname: '**system**',
+                        strMessage: "あなたのid:" + assignId,
+                        strDate: strNow
+                    }
+                    io.to(socket.id).emit('spread message', objMessage);
+                    // console.log('fields: ', fields);
+                    io.to(socket.id).emit('Reg OK', assignId);
+                });
+            }
+        )
+
+    });
 
 // 公開フォルダの指定
-app.use( express.static( __dirname + '/public' ) );
+app.use(express.static(__dirname + '/public'));
 
 // サーバーの起動
 server.listen(
     PORT,
-    () =>
-    {
-        console.log( 'Server on port %d', PORT );
-    } );
+    () => {
+        console.log('Server on port %d', PORT);
+    });
 
 // 関数
 // 数字を２桁の文字列に変換
 const toDoubleDigitString =
-    ( num ) =>
-    {
-        return ( "0" + num ).slice( -2 );   // slice( -2 )で後ろから２文字取り出す。
+    (num) => {
+        return ("0" + num).slice(-2);   // slice( -2 )で後ろから２文字取り出す。
     };
 
 // 時刻文字列の作成（書式は「YY/DD/MM hh:mm ss」）
 const makeTimeString =
-    ( time ) =>
-    {
-        return toDoubleDigitString( time.getFullYear() ) + '/' + toDoubleDigitString( time.getMonth() + 1 ) + '/' + toDoubleDigitString( time.getDate() )
-            + ' ' + toDoubleDigitString( time.getHours() ) + ':' + toDoubleDigitString( time.getMinutes() ) + ' ' + toDoubleDigitString( time.getSeconds() );
+    (time) => {
+        return toDoubleDigitString(time.getFullYear()) + '/' + toDoubleDigitString(time.getMonth() + 1) + '/' + toDoubleDigitString(time.getDate())
+            + ' ' + toDoubleDigitString(time.getHours()) + ':' + toDoubleDigitString(time.getMinutes()) + ' ' + toDoubleDigitString(time.getSeconds());
     }
